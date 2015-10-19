34f226331b71520f2b7334b4b0113794113d8ea0
Michael Mayer <michael@liquidbytes.net>
2015-10-19 12:27:44 +0200

    Added SampleCommandTest

---
+++ src/Sampler/Tests/Command/SampleCommandTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Sampler\Tests\Input;
+
+use TestTools\TestCase\UnitTestCase;
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Output\BufferedOutput;
+
+class SampleCommandTest extends UnitTestCase
+{
+    /**
+     * @var \Sampler\Command\SampleCommand
+     */
+    protected $command;
+
+    public function setUp()
+    {
+        $this->command = $this->get('command.sample');
+    }
+
+    public function testExecuteInternal()
+    {
+        $input = new ArrayInput(array('--size' => 10, '--input' => 'internal'), $this->command->getDefinition());
+        $output = new BufferedOutput();
+
+        $this->command->run($input, $output);
+
+        $result = $output->fetch();
+
+        $this->assertEquals(11, strlen($result)); // 10 + 1 for the new line
+    }
+
+    public function testExecuteRandomOrg()
+    {
+        $input = new ArrayInput(array('--size' => 50, '--input' => 'random.org'), $this->command->getDefinition());
+        $output = new BufferedOutput();
+
+        $this->command->run($input, $output);
+
+        $result = $output->fetch();
+
+        $this->assertEquals(51, strlen($result)); // 10 + 1 for the new line
+    }
+}
